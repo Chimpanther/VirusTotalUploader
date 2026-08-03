18e8c8861b87f9a5f361f32d84dd78328bcdfbf6
Chimpanther <132769724+Chimpanther@users.noreply.github.com>
2026-08-03 02:37:57 +0000

    🧪 Add tests for Utils hashing functions

---
+++ uploader/uploader.Tests/UtilsTests.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+using System.IO;
+using uploader;
+
+namespace uploader.Tests
+{
+    public class UtilsTests
+    {
+        [Test]
+        public void GetSHA1_ReturnsCorrectHash()
+        {
+            var tempFile = Path.GetTempFileName();
+            try
+            {
+                File.WriteAllBytes(tempFile, System.Text.Encoding.ASCII.GetBytes("test"));
+                var result = Utils.GetSHA1(tempFile);
+                Assert.That(result, Is.EqualTo("A94A8FE5CCB19BA61C4C0873D391E987982FBBD3"));
+            }
+            finally
+            {
+                File.Delete(tempFile);
+            }
+        }
+
+        [Test]
+        public void GetMD5_ReturnsCorrectHash()
+        {
+            var tempFile = Path.GetTempFileName();
+            try
+            {
+                File.WriteAllBytes(tempFile, System.Text.Encoding.ASCII.GetBytes("test"));
+                var result = Utils.GetMD5(tempFile);
+                Assert.That(result, Is.EqualTo("098F6BCD4621D373CADE4E832627B4F6"));
+            }
+            finally
+            {
+                File.Delete(tempFile);
+            }
+        }
+
+        [Test]
+        public void GetSHA256_ReturnsCorrectHash()
+        {
+            var tempFile = Path.GetTempFileName();
+            try
+            {
+                File.WriteAllBytes(tempFile, System.Text.Encoding.ASCII.GetBytes("test"));
+                var result = Utils.GetSHA256(tempFile);
+                Assert.That(result, Is.EqualTo("9F86D081884C7D659A2FEAA0C55AD015A3BF4F1B2B0B822CD15D6C15B0F00A08"));
+            }
+            finally
+            {
+                File.Delete(tempFile);
+            }
+        }
+    }
+}
