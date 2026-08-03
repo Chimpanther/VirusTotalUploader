c5e24cc08d1bd23f0979ef995244286518715438
Chimpanther <132769724+Chimpanther@users.noreply.github.com>
2026-08-03 13:10:05 +0000

    perf: optimize upload workflow to use parallel async tasks

--- fix_duplicate_using.py
+++
@@ -1,7 +0,0 @@
-with open("uploader/uploader/UploadForm.cs", "r", encoding="utf-8") as f:
-    content = f.read()
-
-content = content.replace("using System.Windows.Forms;\nusing System.Threading;\nusing System.Threading.Tasks;", "using System.Windows.Forms;")
-
-with open("uploader/uploader/UploadForm.cs", "w", encoding="utf-8") as f:
-    f.write(content)
