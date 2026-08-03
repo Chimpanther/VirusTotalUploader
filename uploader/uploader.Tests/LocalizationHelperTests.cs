70beaa6e163e258edafc30b6c27fce3d857fbba8
Chimpanther <132769724+Chimpanther@users.noreply.github.com>
2026-08-03 02:39:32 +0000

    test: add LocalizationHelper.Export unit test

---
+++ uploader/uploader.Tests/LocalizationHelperTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Newtonsoft.Json;
+using uploader;
+
+namespace uploader.Tests
+{
+    [TestClass]
+    public class LocalizationHelperTests
+    {
+        [TestMethod]
+        public void Export_CreatesValidJsonFile()
+        {
+            // Arrange
+            string exportFileName = "export.json";
+            if (File.Exists(exportFileName))
+            {
+                File.Delete(exportFileName);
+            }
+
+            // Act
+            LocalizationHelper.Export();
+
+            // Assert
+            Assert.IsTrue(File.Exists(exportFileName), "The export.json file should be created.");
+
+            string jsonContent = File.ReadAllText(exportFileName);
+            var deserialized = JsonConvert.DeserializeObject<LocalizationBase>(jsonContent);
+
+            Assert.IsNotNull(deserialized, "The JSON content should be deserializable to LocalizationBase.");
+            Assert.AreEqual("Drag file here", deserialized.MainForm_DragFile, "The default values should be preserved.");
+            Assert.AreEqual("More", deserialized.MainForm_More, "The default values should be preserved.");
+            Assert.AreEqual("Settings", deserialized.SettingsForm_Title, "The default values should be preserved.");
+
+            // Clean up
+            File.Delete(exportFileName);
+        }
+    }
+}
