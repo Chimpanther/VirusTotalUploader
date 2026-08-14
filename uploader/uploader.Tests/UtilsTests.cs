2caacea57b010db7299c2f61c3eb1b641eb766ec
Magnus Aune <dev@chimpanther.com>
2026-08-14 08:59:42 +0200

    fix: combine master and PR test coverage in UtilsTests (xUnit)

--- uploader/uploader.Tests/UtilsTests.cs
+++ uploader/uploader.Tests/UtilsTests.cs
@@ -1,11 +1,44 @@
-using Xunit;
+using System;
 using System.IO;
 using uploader;
+using Xunit;
 
 namespace uploader.Tests
 {
     public class UtilsTests
     {
+        [Fact]
+        public void GetMD5_NonExistentFile_ThrowsFileNotFoundException()
+        {
+            // Arrange
+            string fakePath = "this_file_does_not_exist.txt";
+
+            // Act & Assert
+            Assert.Throws<FileNotFoundException>(() => Utils.GetMD5(fakePath));
+        }
+
+        [Fact]
+        public void GetMD5_ValidFile_ReturnsCorrectHash()
+        {
+            // Arrange
+            string tempFile = Path.GetTempFileName();
+            try
+            {
+                File.WriteAllText(tempFile, "hello world");
+
+                // Act
+                string hash = Utils.GetMD5(tempFile);
+
+                // Assert
+                // MD5 of "hello world" is 5EB63BBBE01EEED093CB22BB8F5ACDC3
+                Assert.Equal("5EB63BBBE01EEED093CB22BB8F5ACDC3", hash);
+            }
+            finally
+            {
+                File.Delete(tempFile);
+            }
+        }
+
         [Fact]
         public void GetSHA1_ReturnsCorrectHash()
         {
@@ -15,22 +48,6 @@ namespace uploader.Tests
                 File.WriteAllBytes(tempFile, System.Text.Encoding.ASCII.GetBytes("test"));
                 var result = Utils.GetSHA1(tempFile);
                 Assert.Equal("A94A8FE5CCB19BA61C4C0873D391E987982FBBD3", result);
-            }
-            finally
-            {
-                File.Delete(tempFile);
-            }
-        }
-
-        [Fact]
-        public void GetMD5_ReturnsCorrectHash()
-        {
-            var tempFile = Path.GetTempFileName();
-            try
-            {
-                File.WriteAllBytes(tempFile, System.Text.Encoding.ASCII.GetBytes("test"));
-                var result = Utils.GetMD5(tempFile);
-                Assert.Equal("098F6BCD4621D373CADE4E832627B4F6", result);
             }
             finally
             {
